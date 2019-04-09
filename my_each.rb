87fb2f062910fcee4ec6c7bdb9d3b42e6eed0103
Jasterix C <loune.calixte@gmail.com>
2019-04-09 20:38:55 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -13,7 +13,7 @@ end
 #at this point, I got a no block given (yield) error on repl
 
 #people using the function
-my_each(collection) do |i|
-  puts i
-end
+# my_each(collection) do |i|
+#   puts i
+# end
 
